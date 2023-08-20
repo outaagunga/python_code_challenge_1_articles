dc6dd1a687e6657ee415a2118e858a2194929e54
outaagunga <jeconiaagunga@gmail.com>
2023-08-20 17:40:45 +0300

    Adjusted the test

--- lib/test.py
+++ lib/test.py
@@ -11,12 +11,16 @@ def test_author_name():
 def test_magazine_name():
     magazine = Magazine("Adventist Magazine", "Health")
     assert magazine.name() == "Adventist Magazine"
+    assert magazine.category() == "Health" # trial to see if it passing
+ 
+
 
 def test_article_title():
     author = Author("Outa Agunga")
     magazine = Magazine("Adventist Magazine", "Health")
     article = Article(author, magazine, "Healthy")
     assert article.title() == "Healthy"
+ 
 
 def test_author_add_article():
     author = Author("Outa Agunga")
@@ -35,3 +39,14 @@ def test_author_add_article():
     assert len(author.articles()) == 3
 
 # ... Add more test functions for other methods ...
+# def test_author_list_articles():
+#     author = Author("Outa Agunga")
+#     magazine = Magazine("Adventist Magazine", "Health")
+
+#     author.add_article(magazine, "Healthy")
+#     author.add_article(magazine, "Vegeterian Way")
+#     author.add_article(magazine, "Natural Way")
+
+#     expected_articles = ["Healthy", "Vegeterian Way", "Natural Way"]
+#     article_titles = [article.title() for article in author.articles()]
+#     assert article_titles == expected_articles
